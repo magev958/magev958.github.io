258daa8f689de91e3aa576d49d6a572292c05416
Magnus <15162054+magev958@users.noreply.github.com>
2022-03-13 22:34:17 +0100

    Add files via upload

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,39 +1,37 @@
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline_v01';
+const OFFLINE_VERSION = 2;
+const CACHE_NAME = 'offline_v02';
 const OFFLINE_URL = '404.html';
-const appShellFiles = [
-	"favicon.ico",
-    "404.html",
-    "distribution.html",
-    "index.html",
-    "investment.html",
-    "latest.html",
-    "thanks.html",
-    "transactions.html",
-	"manifest.webmanifest",
-	"CascadingStyleSheets/styles.css",
-	"Images/apple-touch-icon.png",
-	"Images/favicon-128x128.png",
-	"Images/icon.svg",
-	"Images/icon-192.png",
-	"Images/icon-512.png",
-    "JavaScript/jQuery-3.6.0.slim.min.js",
-	"Javascript/RGraph.common.core.js",
-	"Javascript/RGraph.common.sheets.js",
-	"Javascript/RGraph.common.key.js",
-	"Javascript/RGraph.common.dynamic.js",
-	"Javascript/RGraph.common.tooltips.js",
-	"Javascript/RGraph.meter.js",
-	"Javascript/RGraph.pie.js"
-];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil((async() => {
-        const cache = await caches.open(CACHE_NAME);
-        await cache.addAll(new Request(appShellFiles, {
-            cache: 'reload'
-        }));
-    })());
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('offline_v02').then(function(cache) {
+      return cache.addAll([
+        'favicon.ico',
+		'404.html',
+		'distribution.html',
+		'index.html',
+		'investment.html',
+		'latest.html',
+		'thanks.html',
+		'transactions.html',
+		'manifest.webmanifest',
+		'CascadingStyleSheets/styles.css',
+		'Images/apple-touch-icon.png',
+		'Images/favicon-128x128.png',
+		'Images/icon.svg',
+		'Images/icon-192.png',
+		'Images/icon-512.png',
+		'JavaScript/jQuery-3.6.0.slim.min.js',
+		'Javascript/RGraph.common.core.js',
+		'Javascript/RGraph.common.sheets.js',
+		'Javascript/RGraph.common.key.js',
+		'Javascript/RGraph.common.dynamic.js',
+		'Javascript/RGraph.common.tooltips.js',
+		'Javascript/RGraph.meter.js',
+		'Javascript/RGraph.pie.js',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', (event) => {
