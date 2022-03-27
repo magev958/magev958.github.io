0b0ff67f4891d36df1ce010d1724831460c89fb8
Magnus <15162054+magev958@users.noreply.github.com>
2022-03-27 06:04:50 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,28 @@
-self.addEventListener('install', function (event) {
-	event.waitUntil(caches.open('offline_v01').then(function (cache) {
-		cache.add(new Request([
-			'404.html',
-			'CascadingStyleSheets/styles.css',
-			'Images/favicon-128x128.png'
-			]));
-		return;
-	}));
-});
-self.addEventListener('fetch', function (event) {
-	var request = event.request;
-	if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-	if (request.headers.get('Accept').includes('text/html')) {
-		event.respondWith(
-			fetch(request).then(function (response) {
-				return response;
-			}).catch(function (error) {
-				return caches.match('404.html');
-			})
-		);
-	}
+const version = '1';
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(`static-${version}`)
+      .then((cache) =>
+        cache.addAll([
+          '/',
+          '/favicon.ico',
+          '/404.html',
+          '/distribution.html',
+          '/index.html',
+          '/investment.html',
+          '/latest.html',
+          '/thanks.html',
+          '/transactions.html',
+          '/manifest.webmanifest',
+          '/CascadingStyleSheets/styles.css',
+          '/Images/apple-touch-icon.png',
+          '/Images/favicon-128x128.png',
+          '/Images/icon.svg',
+          '/Images/icon-192.png',
+          '/Images/icon-512.png'
+        ]),
+      ),
+  );
 });
