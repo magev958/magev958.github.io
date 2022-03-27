9dc89c29c0eb6be972f33125975b2ab4cb7480ba
Magnus <15162054+magev958@users.noreply.github.com>
2022-03-27 23:13:11 +0200

    Create serviceWorker_working.js

---
+++ serviceWorker_working.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(async function() {
+    const cache = await caches.open('mysite-static-v3');
+    await cache.addAll([
+          '/',
+          '/favicon.ico',
+          '/404.html',
+          '/distribution.html',
+          '/index.html',
+          '/investments.html',
+          '/latest.html',
+          '/thanks.html',
+          '/transactions.html',
+          '/manifest.webmanifest',
+          '/CascadingStyleSheets/styles.css',
+          '/Images/apple-touch-icon.png',
+          '/Images/favicon-128x128.png',
+          '/Images/icon.svg',
+          '/Images/icon-192.png',
+          '/Images/icon-512.png'
+    ]);
+  }());
+});
