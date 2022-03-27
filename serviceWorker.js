b2c936bd1c7c4a3fff6dfa87934fa61f8c95cd0b
Magnus <15162054+magev958@users.noreply.github.com>
2022-03-27 06:43:36 +0200

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,21 +3,6 @@ self.addEventListener('install', (event) => {
     const cache = await caches.open('mysite-static-v3');
     await cache.addAll([
           '/',
-          '/favicon.ico',
-          '/404.html',
-          '/distribution.html',
-          '/index.html',
-          '/investment.html',
-          '/latest.html',
-          '/thanks.html',
-          '/transactions.html',
-          '/manifest.webmanifest',
-          '/CascadingStyleSheets/styles.css',
-          '/Images/apple-touch-icon.png',
-          '/Images/favicon-128x128.png',
-          '/Images/icon.svg',
-          '/Images/icon-192.png',
-          '/Images/icon-512.png'
     ]);
   }());
 });
